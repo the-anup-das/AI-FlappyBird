a57a4bada7dccac2afd8e67fd052cac7ba2c8dc5
Anup Das <anup.das17@yahoo.com>
2020-10-28 21:09:34 +0530

    making the bird move

--- flappy_bird.py
+++ flappy_bird.py
@@ -37,5 +37,65 @@ class Bird:
         self.tick_count+=1
 
         d=self.vel*self.tick_count+1.5*self.tick_count**2
+
+        if d>=16: #terminal velocity
+            d=16
+        elif d<0:
+            d-=2
+
+        self.y=self.y+d
+
+        if d<0 or self.y<self.height+50:    #if going upwards
+            if self.tilt<self.MAX_ROTATION:
+                self.tilt=self.MAX_ROTATION
+        else:
+            if self.tilt>-90:
+                self.tilt-=self.ROT_VEL
+    
+    def draw(self, win):
+        self.img_count+=1
+
+        if self.img_count<self.ANIMATION_TIME:
+            self.img=self.IMGS[0]
+        elif self.img_count<self.ANIMATION_TIME*2:
+            self.img=self.IMGS[1]
+        elif self.img_count<self.ANIMATION_TIME*3:
+            self.img=self.IMGS[2]
+        elif self.img_count<self.ANIMATION_TIME*4:
+            self.img=self.IMGS[1]
+        elif self.img_count==self.ANIMATION_TIME*4+1:
+            self.img=self.IMGS[0]
+            self.img_count==0
+        
+        if self.tilt<=-80:
+            self.img=self.IMGS[1]
+            self.img_count=self.ANIMATION_TIME*2
+
+        rotated_image=pygame.transform.rotate(self.img,self.tilt)
+        new_rect=rotated_image.get_rect(center=self.img.get_rect(topLeft=(self.x,self.y)).center)
+        win.blit(rotated_image,new_rect.topleft)
+    
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+def draw_window(win,bird):
+    win.blit(BG_IMG,(0,0))
+    bird.draw(win)
+    pygame.display.update()
+
+def main():
+    bird=Bird(200,200)
+    win=pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+
+    run=True
+    while run:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=False
+        draw_window(win,bird)
+    
+    pygame.quit()
+    quit()
+
 while True:
     Bird.move()
