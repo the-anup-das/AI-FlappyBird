1c8d5f8210bb2915a94f13d35f475b2ef14b9569
Anup Das <anup.das17@yahoo.com>
2020-10-30 10:39:14 +0530

    adding requirements file

--- flappy_bird.py
+++ flappy_bird.py
@@ -10,7 +10,7 @@ WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 GEN=0
-DRAW_LINES=False
+DRAW_LINES=True
 
 BIRD_IMGS=[pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird1.png'))),pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird3.png')))]
 PIPE_IMG=pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
